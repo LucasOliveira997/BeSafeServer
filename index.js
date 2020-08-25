2a3c5a532d4ce52328d06db85a4d53001ef8afdf
LucasCastro997 <lucas.c_julien@outlook.com>
2020-08-25 16:46:10 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+
+//Socket Logic
+const socketio = require('socket.io')(http)
+
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data)
+    })
+})
+
+http.listen(process.env.PORT)
